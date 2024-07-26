c5d9afd9847799d66e8009975f53d35cff872a74
Whiser <117631755+wea-the@users.noreply.github.com>
2024-07-26 08:53:14 +0800

    Update Slap-Battles.lua

--- Files/Slap-Battles.lua
+++ Files/Slap-Battles.lua
@@ -3,6 +3,6 @@ shared.createSafeSpot = function()
     Part.Name = "SafeSpot"
     Part.Anchored = true
     Part.Position = Vector3.new(-48237.4219, 5900.68115, -26118.4941) 
-    Part.Size = Vector3.new(500, 500, 10) 
+    Part.Size = Vector3.new(1000, 10, 1000) 
     Part.Parent = workspace
 end
