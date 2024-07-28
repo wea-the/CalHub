8208224493f2270237f935939894db61aa856fb6
Whiser <117631755+wea-the@users.noreply.github.com>
2024-07-28 14:40:12 +0800

    Update Load-Script.lua

--- Load-Script.lua
+++ Load-Script.lua
@@ -1,25 +1,19 @@
 --// This is where all the scripts for the supported games located. All the links here are protected. \\
 local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
-local core = game:GetService("CoreGui")
-
-local gameList = {
-    [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Eternal-Bob.lua'))()",
-    [2380077519] = "loadstring(game:HttpGet('https://pastebin.com/raw/ejSuqKqh'))()"
-}
-
 if teleportFunc then
     teleportFunc(
         if not game:IsLoaded() then
-            local mes = Instance.new("Message")
-            mes.Parent = core
-            mes.Text = "CalHub is Waiting for the Game to load..."
             game.Loaded:Wait()
-            mes:Destroy()
         end
         repeat task.wait() until
         game.Players.LocalPlayer
     )
 end
+
+local gameList = {
+    [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Eternal-Bob.lua'))()",
+    [2380077519] = "loadstring(game:HttpGet('https://pastebin.com/raw/ejSuqKqh'))()"
+}
 
 if gameList[game.GameId] ~= nil then
     loadstring(gameList[tonumber(game.GameId)])()
