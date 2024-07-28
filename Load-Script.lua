b08762e81cb7a4b26d2e2f5c715c98e950c1cc79
Whiser <117631755+wea-the@users.noreply.github.com>
2024-07-28 14:58:21 +0800

    Update Load-Script.lua

--- Load-Script.lua
+++ Load-Script.lua
@@ -1,16 +1,12 @@
 -- This is where all the scripts for the supported games located. All the links here are protected.
-local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
-if teleportFunc then
-    teleportFunc(
-        if not game:IsLoaded() then
-            game.Loaded:Wait()
-        end
-        repeat task.wait() until
-        game.Players.LocalPlayer
-    )
+if not game:IsLoaded() then
+    game.Loaded:Wait()
 end
+repeat task.wait() until
+game.Players.LocalPlayer
 
 local gameList = {
+    -- Eternal Bob (Slap Battles)
     [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Eternal-Bob.lua'))()",
     [2380077519] = "loadstring(game:HttpGet('https://pastebin.com/raw/ejSuqKqh'))()"
 }
