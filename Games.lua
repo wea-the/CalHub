fff15fee4140857ad84af8d5fe436aec7b61eeef
Whiser <117631755+wea-the@users.noreply.github.com>
2024-07-28 13:49:15 +0800

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,28 +1,28 @@
 --// This is where all the scripts for the supported games located. All the links here are protected. \\
+local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
+local core = game:GetService("CoreGui")
 
 local games = {
     [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Eternal-Bob.lua'))()",
     [6403373529] = "loadstring(game:HttpGet('https://pastebin.com/raw/ejSuqKqh'))()"
 }
 
-local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
-local core = game:GetService("CoreGui")
+if teleportFunc then
+    teleportFunc(
+        if not game:IsLoaded() then
+            local mes = Instance.new("Message")
+            mes.Parent = core
+            mes.Text = "CalHub is Waiting for the Game to load..."
+            game.Loaded:Wait()
+            mes:Destroy()
+        end
+        repeat task.wait() until
+        game.Players.LocalPlayer
+    )
+end
+
 if games[game.PlaceId] ~= nil then
-    if teleportFunc then
-        teleportFunc(
-            if not game:IsLoaded() then
-                local mes = Instance.new("Message")
-                mes.Parent = core
-                mes.Text = "CalHub is Waiting for the Game to load..."
-                game.Loaded:Wait()
-                mes:Destroy()
-            end
-            repeat task.wait() until
-            game.Players.LocalPlayer
-            wait(0.10)
-            loadstring(games[tonumber(game.PlaceId)])()
-        )
-    end
+    loadstring(games[tonumber(game.PlaceId)])()
 else
     game.Players.LocalPlayer:Kick("Game Not Supported! :(")
 end
