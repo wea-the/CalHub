bec410bb60b0a6daf14df4c6698974968f8d2233
Whiser <117631755+wea-the@users.noreply.github.com>
2024-08-27 14:21:03 +0800

    Add files via upload

---
+++ Files/SlapBattlesFiles.lua
@@ -0,0 +1,8 @@
+shared.createSafeSpot = function()
+    local Part = Instance.new("Part")
+    Part.Name = "SafeSpot"
+    Part.Anchored = true
+    Part.Position = Vector3.new(-48237.4219, 5900.68115, -26118.4941) 
+    Part.Size = Vector3.new(1000, 10, 1000) 
+    Part.Parent = workspace
+end
